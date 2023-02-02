9f3fcbb9893f1f7402ac5612736a12a83514f973
Annas Ali Mian <annas.ali@systemsltd.com>
2023-02-02 18:00:26 +0500

    completed module 7

--- module6/pages/index.js
+++ module6/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
 
@@ -6,6 +7,13 @@ function HomePage() {
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...' 
+        />
+      </Head>
       <EventList items={featuredEvents} />
     </div>
   );
